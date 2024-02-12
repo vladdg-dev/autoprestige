eb4a263a45e85ed58a647c9d7aa1c9414af1850d
psypher1337 <vlad.dragusanu@gmail.com>
2024-02-12 10:05:11 +0200

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,11 @@ import Hero from '@/components/Hero';
 import Catalogue from '@/components/Catalogue';
 import Provider from '@/components/Provider';
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const Home = () => {
   return (
     <main className="overflow-hidden">
+      <Hero />
       <Provider>
-        <Hero />
         <Catalogue />
       </Provider>
     </main>
